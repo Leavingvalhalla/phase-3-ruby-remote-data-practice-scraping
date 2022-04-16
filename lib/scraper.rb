bcd8eb9246be4e8f99c3a010b9d00ec2c6f2c1d5
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-16 12:56:49 -0700

    commit tree f246121d042c7efad77c1dd481637cae74a5a9a9

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css('.card-title-tl')
+puts courses.map {|course| course.text.strip}
